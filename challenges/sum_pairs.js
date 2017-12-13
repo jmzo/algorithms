c1d36e8a17ec2452434a1d856346e6db8a9d26d5
Jim Caratozzolo <jcaratozzolo@auction.com>
2017-12-13 09:49:22 -0800

    refactoring, adding array helpers

--- challenges/sum_pairs.js
+++ challenges/sum_pairs.js
@@ -1,47 +1,3 @@
-function solve (targetValue, lowerLimit, upperLimit, randomizeSet) {
-  // Cast to integers.
-  targetValue = parseInt(targetValue)
-  lowerLimit = parseInt(lowerLimit)
-  upperLimit = parseInt(upperLimit)
-  
-  // Make sure neither have resolved to NaN.
-  let absOfSum = Math.abs(lowerLimit + upperLimit + targetValue)
-  if (absOfSum !== absOfSum) {
-    console.error('arguments must each be a number.')
-    return
-  }
-  
-  // Make sure lowerLimit is less than or equal to upperLimit.
-  if (lowerLimit > upperLimit) {
-    console.error('lowerLimit argument must be less than or equal to upperLimit.')
-    return
-  }
-  
-  let array = generateArray(lowerLimit, upperLimit, randomizeSet)
-  let pairs = findSumPairs(targetValue, array)
-  console.log(pairs)
-}
-
-function generateArray (lowerLimit, upperLimit, randomizeSet) {
-  let array = []
-  for (let i = lowerLimit; i <= upperLimit; i++) {
-    array.push(i)
-  }
-  array = randomizeSet ? randomizeArray(array) : array
-  return array
-}
-
-function randomizeArray (array) {
-  let randomized = []
-  while (array.length) {
-    let index = Math.floor(Math.random() * array.length)
-    randomized.push(array[index])
-    array.splice(index, 1)
-  }
-  console.log('RANDOM', randomized)
-  return randomized
-}
-
 function findSumPairs (targetValue, array) {
   let sumPairs = []
   let set = new Set()
@@ -54,5 +10,5 @@ function findSumPairs (targetValue, array) {
 }
 
 module.exports = {
-  solve
+  findSumPairs
 }
---
+++ helpers/arrays.js
@@ -0,0 +1,62 @@
+function generateIntegerArray (lowerLimit, upperLimit, arraySize) {
+  let result = checkArguments(arguments, [
+    ['lowerLimit', 'number'],
+    ['upperLimit', 'number'],
+    ['arraySize',  'number']
+  ])
+  
+  // Cast to integers.
+  lowerLimit = parseInt(lowerLimit)
+  upperLimit = parseInt(upperLimit)
+  arraySize = parseInt(arraySize)
+  
+  // Make sure neither have resolved to NaN.
+  let absOfSum = Math.abs(lowerLimit + upperLimit + arraySize)
+  if (absOfSum !== absOfSum) {
+    console.error('arguments must each be a number.')
+    return
+  }
+  
+  // Make sure lowerLimit is less than or equal to upperLimit.
+  if (lowerLimit > upperLimit) {
+    console.error('lowerLimit argument must be less than or equal to upperLimit.')
+    return
+  }
+  
+  let array = []
+  for (let i = 0; i < arraySize; i++) {
+    let spanLength = upperLimit - lowerLimit
+    let number = Math.floor(Math.random() * (spanLength + 1)) + lowerLimit
+    array.push(number)
+  }
+  return array
+}
+
+function checkArguments () {
+  let caller = arguments.caller || checkArguments.caller
+  let msgs = []
+  let argsToValidate = arguments[0]
+  let validators = arguments[1]
+  for (let i = 0; i < validators.length; i++) {
+    if (typeof argsToValidate[i] !== validators[i][1] ) msgs.push(`Argument '${validators[i][0]}' of function '${caller.name}' must be of type '${validators[i][1]}'.`)
+  }
+  if (msgs.length) console.warn(JSON.stringify(msgs, null, 4))
+}
+
+/*
+Consider using Fisher-Yates https://bost.ocks.org/mike/shuffle/
+*/
+function shuffleArray (array) {
+  let shuffled = []
+  while (array.length) {
+    let index = Math.floor(Math.random() * array.length)
+    shuffled.push(array[index])
+    array.splice(index, 1)
+  }
+  return shuffled
+}
+
+module.exports = {
+  generateIntegerArray,
+  shuffleArray
+}
